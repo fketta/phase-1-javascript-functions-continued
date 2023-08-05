d5deb7f3c117851fa5ae910f2f0effd21acbcf0a
Fella Kettani <fellakettani@gmail.com>
2023-08-05 19:55:06 +0100

    finished assignement

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate")
+{
+    return "This Saturday, I want to " + activity + "!";
+}
+console.log(saturdayFun())
+
+function mondayWork (work = "go to the office")
+{
+    return "This Monday, I will " + work + ".";
+}
+console.log(mondayWork())
+
+function wrapAdjective(flair = "*") 
+{
+    return function (adjective = "special") 
+    {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
